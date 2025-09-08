3de3764917b8577e679dc9a586ea4f19816b3c72
david teller <ydt009@gmail.com>
2025-09-08 17:21:44 +0300

    add a navbar

--- linkkodcode-client/src/components/application-layout/Haeder.tsx
+++ linkkodcode-client/src/components/application-layout/Haeder.tsx
@@ -1,12 +1,18 @@
 import "../../styles/Header.css";
+import NavBar from "../NavBar";
 
-// the header holds the slogen and the logo, all the time 
+// the header holds the slogen and the logo, all the time
 export default function Header() {
   return (
     <>
       <header className="header">
         <h1 className="slogen">this is the slogen</h1>
-        <img src="https://lh3.googleusercontent.com/drive-storage/AJQWtBMtE5iOXV21OqYseQ7aKan9q2enkM37A8-Z9LSuEdQkGR9HlhSOfQwgEw5DxeX9X823VPfT4BeT7PELMGKLfFh4k55iOfPZKnX03M6NGSKLp7M=w1920-h988?auditContext=forDisplay"  alt="" className="logo" />
+        <img
+          src="https://lh3.googleusercontent.com/drive-storage/AJQWtBMtE5iOXV21OqYseQ7aKan9q2enkM37A8-Z9LSuEdQkGR9HlhSOfQwgEw5DxeX9X823VPfT4BeT7PELMGKLfFh4k55iOfPZKnX03M6NGSKLp7M=w1920-h988?auditContext=forDisplay"
+          alt=""
+          className="logo"
+        />
+        <NavBar />
       </header>
     </>
   );
